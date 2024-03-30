6899c115ab7d3a4016e196843b883bff47a88066
Cahil Foley <cahilfoley2@gmail.com>
2024-03-30 11:51:31 +0800

    feat(Graph): Add full x and y coordinate system for all LEDs and nodes

--- src/system/Graph.h
+++ src/system/Graph.h
@@ -6,58 +6,28 @@
 #include "config.h"
 #include "system/mapping.h"
 
-class Node;
-
-class Strip {
- public:
-  byte index;
-  byte length = STRIP_LED_COUNT;
-  byte channel;
-  byte row;
-  byte column;
-  int channelLEDOffset;
-  int channelOffset;
-  int channelLength;
-  int stripStartLED;
-  int stripEndLED;
-
-  Node *topNode;
-  Node *bottomNode;
-
-  Strip(){};
-  Strip(byte _index) {
-    index = _index;
-    row = stripConfigs[index].row;
-    column = stripConfigs[index].column;
-    channel = stripConfigs[index].channelIndex;
-    channelLength = lengths[channel];
-    channelOffset = channelOffsets[channel];
-    stripStartLED = channelOffset + stripConfigs[index].topLEDIndex;
-    stripEndLED = channelOffset + stripConfigs[index].bottomLEDIndex;
-  }
-
-  /** Get the global LED index for a given index on the strip */
-  int getLEDIndex(int stripIndex) { return map(stripIndex, 0, length - 1, stripEndLED, stripStartLED); }
-
-  /** Merge the provided colour into the target LED weighted by the provided
-   * proportion */
-  void applyColorToLED(CRGB ledColors[TOTAL_LEDS], int led, CRGB color, float proportion) {
-    int ledIndex = getLEDIndex(led);
-    CRGB currentColour = ledColors[ledIndex];
-
-    ledColors[ledIndex].setRGB(qadd8(currentColour.r, color.r * proportion),
-                               qadd8(currentColour.g, color.g * proportion),
-                               qadd8(currentColour.b, color.b * proportion));
-  }
-};
+class Strip;
 
 class Node {
  public:
   byte index;
   Strip *strips[6];
   bool borderNode = false;
+  byte row;
+  byte column;
+  int x;
+  int y;
+
   Node(){};
-  Node(byte index) { this->index = index; }
+  Node(byte index) {
+    this->index = index;
+    this->row = nodeConnections[index].row;
+    this->column = nodeConnections[index].column;
+
+    // Have to add one to the strip LED count to account for the node itself and any verticals in line with it
+    this->x = this->column * (STRIP_LED_COUNT + 1);
+    this->y = this->row * (STRIP_LED_COUNT + 1);
+  }
 
   byte getStripCount() {
     byte count = 0;
@@ -76,10 +46,95 @@ class Node {
   }
 };
 
+struct Coordinate {
+  int x;
+  int y;
+};
+
+struct LEDCoordinate {
+  int x;
+  int y;
+  int globalIndex;
+};
+
+class Strip {
+ public:
+  byte index;
+  byte length = STRIP_LED_COUNT;
+  byte channel;
+  byte row;
+  byte column;
+  StripOrientation orientation;
+  LEDCoordinate leds[STRIP_LED_COUNT];
+  int channelLEDOffset;
+  int channelOffset;
+  int channelLength;
+  int stripStartLED;
+  int stripEndLED;
+
+  Node *topNode;
+  Node *bottomNode;
+
+  Strip(){};
+  Strip(byte _index) {
+    index = _index;
+    row = stripConfigs[index].row;
+    column = stripConfigs[index].column;
+    orientation = stripConfigs[index].orientation;
+    channel = stripConfigs[index].channelIndex;
+    channelLength = lengths[channel];
+    channelOffset = channelOffsets[channel];
+    stripStartLED = channelOffset + stripConfigs[index].topLEDIndex;
+    stripEndLED = channelOffset + stripConfigs[index].bottomLEDIndex;
+  }
+
+  // Calculate the coordinates of each LED in the strip, this should only be called once and should be called before
+  // anything else
+  void calculateLEDPositions() {
+    for (byte i = 0; i < STRIP_LED_COUNT; i++) {
+      auto coordinate = getLEDCoordinates(i);
+      /** Get the global LED index for a given index on the strip, this is the position in the `leds` array. */
+      int ledIndex = map(i, 0, length - 1, stripEndLED, stripStartLED);
+      leds[i] = {coordinate.x, coordinate.y, ledIndex};
+    }
+  }
+
+  /** Merge the provided colour into the target LED weighted by the provided
+   * proportion */
+  void applyColorToLED(CRGB ledColors[TOTAL_LEDS], int led, CRGB color, float proportion) {
+    auto ledCoordinate = leds[led];
+    CRGB currentColour = ledColors[ledCoordinate.globalIndex];
+
+    ledColors[ledCoordinate.globalIndex].setRGB(qadd8(currentColour.r, color.r * proportion),
+                                                qadd8(currentColour.g, color.g * proportion),
+                                                qadd8(currentColour.b, color.b * proportion));
+  }
+
+ private:
+  Coordinate getLEDCoordinates(int stripIndex) {
+    int x;
+    int y;
+    if (this->orientation == vertical) {
+      x = this->bottomNode->x;
+      // The y value is the bottom node's y value plus the strip index * 2 to account for the vertical spacing
+      y = this->topNode->y + (this->length - stripIndex) * 2;
+    } else if (this->orientation == downRight) {
+      x = this->topNode->x + this->length - stripIndex;
+      y = this->bottomNode->y - stripIndex - 1;
+    } else if (this->orientation == downLeft) {
+      x = this->bottomNode->x + stripIndex + 1;
+      y = this->bottomNode->y - stripIndex - 1;
+    }
+
+    return {x, y};
+  }
+};
+
 class Graph {
  public:
   Strip strips[STRIP_COUNT];
   Node nodes[NODE_COUNT];
+  LEDCoordinate *ledCoordinates[xLimit][yLimit];
   // Strip *stripCoords[stripRowCount][stripColumnCount];
 
   Graph() {
@@ -114,6 +169,16 @@ class Graph {
     // Do any post assembly property calculations (like figuring out border nodes)
     for (auto &node : nodes) {
       node.calculateProperties();
+    }
+
+    // Calculate the LED positions for each strip
+    for (auto &strip : strips) {
+      strip.calculateLEDPositions();
+
+      // Populate each of the LEDs into the coordinate grid
+      for (auto &led : strip.leds) {
+        ledCoordinates[led.x][led.y] = &led;
+      }
     }
   }
 };
--- src/system/mapping.h
+++ src/system/mapping.h
@@ -25,29 +25,32 @@ struct NodeConfig {
   byte row;
   byte column;
 };
-NodeConfig nodeConnections[25] = {{{-1, -1, 1, -1, 0, -1}, true, 0, 0},    {{-1, -1, 3, -1, 2, -1}, true, 0, 1},
-                                  {{-1, -1, 5, -1, 4, -1}, true, 0, 2},
+NodeConfig nodeConnections[25] = {{{-1, -1, 1, -1, 0, -1}, true, 0, 5},    {{-1, -1, 3, -1, 2, -1}, true, 0, 3},
+                                  {{-1, -1, 5, -1, 4, -1}, true, 0, 1},
 
-                                  {{-1, 0, 6, 12, -1, -1}, true, 1, 0},    {{-1, 2, 8, 14, 7, 1}, false, 1, 1},
-                                  {{-1, 4, 10, 16, 9, 3}, false, 1, 2},    {{-1, -1, -1, 18, 11, 5}, true, 1, 3},
+                                  {{-1, 0, 6, 12, -1, -1}, true, 1, 6},    {{-1, 2, 8, 14, 7, 1}, false, 1, 4},
+                                  {{-1, 4, 10, 16, 9, 3}, false, 1, 2},    {{-1, -1, -1, 18, 11, 5}, true, 1, 0},
 
-                                  {{-1, 7, -1, 13, -1, 6}, false, 2, 0},   {{-1, 9, -1, 15, -1, 8}, false, 2, 1},
-                                  {{-1, 11, -1, 17, -1, 10}, false, 2, 2},
+                                  {{-1, 7, -1, 13, -1, 6}, false, 2, 5},   {{-1, 9, -1, 15, -1, 8}, false, 2, 3},
+                                  {{-1, 11, -1, 17, -1, 10}, false, 2, 1},
 
-                                  {{12, -1, 19, -1, -1, -1}, true, 3, 0},  {{14, -1, 21, -1, 20, -1}, false, 3, 1},
-                                  {{16, -1, 23, -1, 22, -1}, false, 3, 2}, {{18, -1, -1, -1, 24, -1}, true, 3, 3},
+                                  {{12, -1, 19, -1, -1, -1}, true, 3, 6},  {{14, -1, 21, -1, 20, -1}, false, 3, 4},
+                                  {{16, -1, 23, -1, 22, -1}, false, 3, 2}, {{18, -1, -1, -1, 24, -1}, true, 3, 0},
 
-                                  {{13, 20, 25, 29, -1, 19}, true, 4, 0},  {{15, 22, 27, 31, 26, 21}, false, 4, 1},
-                                  {{17, 24, -1, 33, 28, 23}, true, 4, 2},
+                                  {{13, 20, 25, 29, -1, 19}, true, 4, 5},  {{15, 22, 27, 31, 26, 21}, false, 4, 3},
+                                  {{17, 24, -1, 33, 28, 23}, true, 4, 1},
 
-                                  {{-1, 26, -1, 30, -1, 25}, false, 5, 0}, {{-1, 28, -1, 32, -1, 27}, false, 5, 1},
+                                  {{-1, 26, -1, 30, -1, 25}, false, 5, 4}, {{-1, 28, -1, 32, -1, 27}, false, 5, 2},
 
-                                  {{29, -1, 34, -1, -1, -1}, true, 6, 0},  {{31, -1, 36, -1, 35, -1}, false, 6, 1},
-                                  {{33, -1, -1, -1, 37, -1}, true, 6, 2},
+                                  {{29, -1, 34, -1, -1, -1}, true, 6, 5},  {{31, -1, 36, -1, 35, -1}, false, 6, 3},
+                                  {{33, -1, -1, -1, 37, -1}, true, 6, 1},
 
-                                  {{30, 35, 38, -1, -1, 34}, true, 7, 0},  {{32, 37, -1, -1, 39, 36}, true, 7, 1},
+                                  {{30, 35, 38, -1, -1, 34}, true, 7, 4},  {{32, 37, -1, -1, 39, 36}, true, 7, 2},
 
-                                  {{-1, 39, -1, -1, -1, 38}, true, 8, 0}};
+                                  {{-1, 39, -1, -1, -1, 38}, true, 8, 3}};
+
+const int xLimit = 7 * (STRIP_LED_COUNT + 1);
+const int yLimit = 9 * (STRIP_LED_COUNT + 1);
 
 enum StripOrientation {
   downLeft = 0,
@@ -63,41 +66,41 @@ struct StripConfig {
   uint16_t bottomLEDIndex;
   byte row;
   byte column;
-  // StripOrientation orientation;
+  StripOrientation orientation;
 };
 
 // First member: Strip number
 // Second: LED index closer to ceiling
 // Third: LED index closer to floor
 StripConfig stripConfigs[40] = {
-    {2, 0, 3, headof(3), tailof(3), 0, 5},   {2, 0, 4, tailof(2), headof(2), 0, 4},
-    {1, 1, 4, headof(10), tailof(10), 0, 3}, {1, 1, 5, tailof(9), headof(9), 0, 2},
-    {1, 2, 5, headof(4), tailof(4), 0, 1},   {1, 2, 6, tailof(3), headof(3), 0, 0},
+    {2, 0, 3, headof(3), tailof(3), 0, 5, downRight},   {2, 0, 4, tailof(2), headof(2), 0, 4, downLeft},
+    {1, 1, 4, headof(10), tailof(10), 0, 3, downRight}, {1, 1, 5, tailof(9), headof(9), 0, 2, downLeft},
+    {1, 2, 5, headof(4), tailof(4), 0, 1, downRight},   {1, 2, 6, tailof(3), headof(3), 0, 0, downLeft},
 
-    {2, 3, 7, tailof(6), headof(6), 1, 5},   {3, 4, 7, tailof(11), headof(11), 1, 4},
-    {1, 4, 8, headof(11), tailof(11), 1, 3}, {1, 5, 8, tailof(8), headof(8), 1, 2},
-    {1, 5, 9, headof(12), tailof(12), 1, 1}, {0, 6, 9, tailof(11), headof(11), 1, 0},
+    {2, 3, 7, tailof(6), headof(6), 1, 5, downLeft},    {3, 4, 7, tailof(11), headof(11), 1, 4, downRight},
+    {1, 4, 8, headof(11), tailof(11), 1, 3, downLeft},  {1, 5, 8, tailof(8), headof(8), 1, 2, downRight},
+    {1, 5, 9, headof(12), tailof(12), 1, 1, downLeft},  {0, 6, 9, tailof(11), headof(11), 1, 0, downRight},
 
-    {2, 3, 10, headof(4), tailof(4), 2, 6},  {3, 7, 14, tailof(10), headof(10), 2, 5},
-    {2, 4, 11, tailof(1), headof(1), 2, 4},  {1, 8, 15, tailof(7), headof(7), 2, 3},
-    {1, 5, 12, headof(5), tailof(5), 2, 2},  {0, 9, 16, tailof(10), headof(10), 2, 1},
-    {1, 6, 13, tailof(2), headof(2), 2, 0},
+    {2, 3, 10, headof(4), tailof(4), 2, 6, vertical},   {3, 7, 14, tailof(10), headof(10), 2, 5, vertical},
+    {2, 4, 11, tailof(1), headof(1), 2, 4, vertical},   {1, 8, 15, tailof(7), headof(7), 2, 3, vertical},
+    {1, 5, 12, headof(5), tailof(5), 2, 2, vertical},   {0, 9, 16, tailof(10), headof(10), 2, 1, vertical},
+    {1, 6, 13, tailof(2), headof(2), 2, 0, vertical},
 
-    {2, 10, 14, headof(5), tailof(5), 3, 5}, {3, 11, 14, tailof(4), headof(4), 3, 4},
-    {3, 11, 15, headof(5), tailof(5), 3, 3}, {0, 12, 15, headof(5), tailof(5), 3, 2},
-    {0, 12, 16, tailof(4), headof(4), 3, 1}, {1, 13, 16, tailof(1), headof(1), 3, 0},
+    {2, 10, 14, headof(5), tailof(5), 3, 5, downLeft},  {3, 11, 14, tailof(4), headof(4), 3, 4, downRight},
+    {3, 11, 15, headof(5), tailof(5), 3, 3, downLeft},  {0, 12, 15, headof(5), tailof(5), 3, 2, downRight},
+    {0, 12, 16, tailof(4), headof(4), 3, 1, downLeft},  {1, 13, 16, tailof(1), headof(1), 3, 0, downRight},
 
-    {3, 14, 17, tailof(9), headof(9), 4, 3}, {0, 15, 17, headof(6), tailof(6), 4, 2},
-    {1, 15, 18, tailof(6), headof(6), 4, 1}, {0, 16, 18, tailof(9), headof(9), 4, 0},
+    {3, 14, 17, tailof(9), headof(9), 4, 3, downLeft},  {0, 15, 17, headof(6), tailof(6), 4, 2, downRight},
+    {1, 15, 18, tailof(6), headof(6), 4, 1, downLeft},  {0, 16, 18, tailof(9), headof(9), 4, 0, downRight},
 
-    {3, 14, 19, tailof(3), headof(3), 5, 4}, {3, 17, 22, tailof(8), headof(8), 5, 3},
-    {3, 15, 20, headof(6), tailof(6), 5, 2}, {0, 18, 23, tailof(8), headof(8), 5, 1},
-    {0, 16, 21, tailof(3), headof(3), 5, 0},
+    {3, 14, 19, tailof(3), headof(3), 5, 4, vertical},  {3, 17, 22, tailof(8), headof(8), 5, 3, vertical},
+    {3, 15, 20, headof(6), tailof(6), 5, 2, vertical},  {0, 18, 23, tailof(8), headof(8), 5, 1, vertical},
+    {0, 16, 21, tailof(3), headof(3), 5, 0, vertical},
 
-    {3, 19, 22, tailof(2), headof(2), 6, 3}, {3, 20, 22, headof(7), tailof(7), 6, 2},
-    {0, 20, 23, headof(7), tailof(7), 6, 1}, {0, 21, 23, tailof(2), headof(2), 6, 0},
+    {3, 19, 22, tailof(2), headof(2), 6, 3, downLeft},  {3, 20, 22, headof(7), tailof(7), 6, 2, downRight},
+    {0, 20, 23, headof(7), tailof(7), 6, 1, downLeft},  {0, 21, 23, tailof(2), headof(2), 6, 0, downRight},
 
-    {3, 22, 24, tailof(1), headof(1), 7, 1}, {0, 23, 24, tailof(1), headof(1), 7, 0},
+    {3, 22, 24, tailof(1), headof(1), 7, 1, downLeft},  {0, 23, 24, tailof(1), headof(1), 7, 0, downRight},
 };
 
 const byte stripRowCount = 8;
